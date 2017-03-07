cc477f2e29b6d3a7ed4843fc69c16711e31e1a99
Michael Chirico <MichaelChirico4@gmail.com>
2017-03-07 18:52:15 -0500

    finish subsetting logic, and split off data cleaning to separate file

--- wisconsin_pay_scales_creator.R
+++ wisconsin_pay_scales_creator.R
@@ -5,7 +5,7 @@
 #August 23, 2015
 
 ###############################################################################
-#                   Package Setup & Convenient Functions                      #
+#                               Package Setup                                 #
 ###############################################################################
 rm(list = ls(all = TRUE))
 gc()
@@ -16,129 +16,6 @@ library(cobs)
 library(parallel)
 library(quantmod)
 library(RPushbullet)
-
-#Given a stream of income in periods 1,...,T
-#  return the discounted sum of future income
-#  from t to T on for each period t
-discounted_earnings = function(x, r = .05){
-  TT = length(x)
-  (upper.tri(matrix(1L, TT, TT), diag = TRUE) *
-    (1/(1+r)) ^ matrix(rep(0L:TT, TT),
-                       nrow = TT+1L, byrow = TRUE)[-(TT+1L), ]) %*% x
-}
-
-###############################################################################
-#                        Data Import & Subset                                 #
-###############################################################################
-
-#Import full matched data created in wisconsin_teacher_data_cleaner.R;
-#  impose sample restrictions to get rid of teachers bound to
-#  cause noise in the contract fitting procedure, as well as teachers
-#  with multiple positions by eliminating all but their highest-FTE positions
-full_data = 
-  fread(data.path %+% "wisconsin_teacher_data_full.csv",
-        select = c("year", "highest_degree", "months_employed", "salary",
-                   "fringe", "category", "position_code", "area",
-                   "full_time_equiv", "days_of_contract", "total_exp_floor",
-                   "teacher_id", "school_fill", "district_fill",
-                   "district_work_type"),
-        #otherwise assumes integer and truncates
-        colClasses = 
-          list(character = c('area', 'school_fill', 'district_fill')))
-
-#Only want full-time teachers
-#  (eliminate 1,924,483 = 46%)
-#   *Mostly categorized as:
-#   "Other Support Staff" [98], "Program Aide" [97],
-#   "Short-term Substitute Teacher" [43], or in major
-#   administrative/staff positions, e.g.
-#   Athletic Coach [77]/Program Coordinator [64]/
-#   Principal [51]/Speech-Language Pathologist [84]/
-#   Plant Maintenance Personnel [72]/Guidance Counselor [54]/
-#   Cafeteria Worker [73]/Clerical Support Staff [68])
-full_data = full_data[position_code == 53L]
-#Focusing on BA/MA scales; eliminate other degrees
-#  (eliminate 21,872 = 1.34%)
-full_data = full_data[highest_degree %in% 4L:5L]
-#Category 1 - Professional, Regular Education
-#  (eliminate 225,186 = 14.94%)
-#  Vast majority dropped are 0: Professional, Special Education
-full_data = full_data[category == 1L]
-#Teachers whose total FTE load is not 100 within a year/district,
-#  and who only taught at one district (some teachers listed as
-#  full-time equivalent 100 at more than one district in a year;
-#  such teachers are quite rare, and perhaps represent some
-#  phasing in phenomenon--they tend to be very young)
-#  (eliminate 152,176 = 10.94%)
-fte100 =
-  full_data[ , if (sum(full_time_equiv, na.rm = TRUE) == 100) TRUE,
-             by = .(teacher_id, year, district_fill)
-             ][ , if (uniqueN(district_fill) == 1L) .SD, 
-                by = .(teacher_id, year)][ , .(teacher_id, year)]
-full_data = full_data[fte100, on = c('teacher_id', 'year')]
-rm(fte100)
-#Eliminate all but the highest-FTE position
-#  (eliminate 228,710 = 18%)
-full_data = full_data[order(full_time_equiv), .SD[.N],
-                      by = .(teacher_id, year)]
-#Focus on Wisconsin Public Schools
-#  (eliminate 14,222 = 1.4%)
-full_data = full_data[district_work_type == '04']
-#Despite efforts to focus on full-time teachers, some,
-#  as per the months_employed construct (used pre-2003)
-#  or the days_of_contract construct (used since), still enter
-#  with what must be less than full-time contracts;
-#  examining, e.g., boxplots of salary against this variable
-#  suggest there is still a systemic deficiency in pay for
-#  those working less than the "full year"; this increasing
-#  relationship appears to stabilize between 8.75 and 10.5 months 
-#  (or 175-195 days contracted), so eliminate those outside these bounds
-#  (eliminate 14,284 = 1.4%)
-full_data = 
-  full_data[(months_employed %between% c(875, 1050) | year > 2003) &
-              (days_of_contract %between% c(175, 195) | year <= 2003)]
-#Seems unlikely a full-time teacher should be
-#  making <10k in a year, so this should be eliminated as erroneous
-#  (eliminate 807 = .08%)
-
-#Now, eliminate schools with insufficient coverage
-yrdsdg = c("year", "district_fill", "highest_degree")
-yrds = c('year', 'district_fill')
-setkeyv(full_data, yrdsdg)
-setindexv(full_data, yrds)
-          
-#In order to use the paired approach to fitting, need
-#  both degree scales represented
-full_data[ , degree_count_flag := uniqueN(highest_degree) == 2L, by = yrds]
-
-#Can't interpolate if there are only 2 or 3
-#  unique experience cells represented
-full_data[ , node_count_flag := uniqueN(total_exp_floor) < 7L, by = yrdsdg]
-
-#Nor if there are too few teachers
-full_data[ , teach_count_flag := .N < 20L, by = yrdsdg]
-
-#Also troublesome when there is little variation in salaries like so:
-full_data[ , sal_scale_flag :=
-             mean(abs(salary - mean(salary))) < 50, by = yrdsdg]
-full_data[ , sal_count_flag := uniqueN(salary) < 5L, by = yrdsdg]
-full_data[ , frn_scale_flag := 
-             mean(abs(salary - mean(fringe))) < 50, by = yrdsdg]
-full_data[ , frn_count_flag := uniqueN(fringe) < 5L, by = yrdsdg]
-
-# Impose the flag on both certification tracks
-#   whenever partially violated
-flgs = paste0(c('node_count', 'teach_count', 'sal_scale',
-                'sal_count', 'frn_scale', 'frn_count'), '_flag')
-full_data[ , (flgs) := lapply(.SD, any), by = yrds, .SDcols = flgs]
-
-#Discard variables not necessary for interpolation
-full_data = 
-  full_data[!(node_count_flag | teach_count_flag |
-                sal_scale_flag | sal_count_flag |
-                frn_scale_flag | frn_count_flag),
-            .(year, district_fill, highest_degree,
-              total_exp_floor, salary, fringe)]
 
 ###############################################################################
 #                             Interpolation                                   #
@@ -256,8 +133,11 @@ pbPost('note', paste('Imputation Done;',
                      proc.time()["elapsed"] - t0,
                      'elapsed.'))
 
-# Post-Fit Clean-up: Real Dollars & Future Values ####
-##Provide deflated wage data
+###############################################################################
+#                     Post-Fit Clean-up: Real-dollar Wages                    #
+###############################################################################
+
+#Provide deflated wage data
 #Note that data values are recorded at the end
 #  of September in each academic year, so,
 #  since I index AY by spring year, we use the

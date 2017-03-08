de4fb0224006774d889ab0c9aff8aa6e51b86b4f
Michael Chirico <MichaelChirico4@gmail.com>
2017-03-07 21:46:18 -0500

    salary & fringe scale flags are redundant, given stricter teach_count_flag; add accumulating counter for paper barplot

--- pay_scales_data_cleaner.R
+++ pay_scales_data_cleaner.R
@@ -1,4 +1,5 @@
 #!/usr/bin/env Rscript
+## @knitr start_read
 #Wisconsin Teacher Project
 #District-Level Pay Scales Data Cleaning
 #Michael Chirico
@@ -31,6 +32,7 @@ full_data =
         colClasses = 
           list(character = c('area', 'district_fill')))
 
+NN = nrow(full_data)
 #Only want full-time teachers
 #  (eliminate 1,924,483 = 46%)
 #   *Mostly categorized as:
@@ -42,13 +44,19 @@ full_data =
 #   Plant Maintenance Personnel [72]/Guidance Counselor [54]/
 #   Cafeteria Worker [73]/Clerical Support Staff [68])
 full_data = full_data[position_code == 53L]
+
+NN = c(NN, nrow(full_data))
 #Focusing on BA/MA scales; eliminate other degrees
 #  (eliminate 21,872 = 1.34%)
 full_data = full_data[highest_degree %in% 4L:5L]
+
+NN = c(NN, nrow(full_data))
 #Category 1 - Professional, Regular Education
 #  (eliminate 225,186 = 14.94%)
 #  Vast majority dropped are 0: Professional, Special Education
 full_data = full_data[category == 1L]
+
+NN = c(NN, nrow(full_data))
 #Teachers whose total FTE load is not 100 within a year/district,
 #  and who only taught at one district (some teachers listed as
 #  full-time equivalent 100 at more than one district in a year;
@@ -61,14 +69,22 @@ fte100 =
              ][ , if (uniqueN(district_fill) == 1L) .SD, 
                 by = .(teacher_id, year)][ , .(teacher_id, year)]
 full_data = full_data[fte100, on = c('teacher_id', 'year')]
+
+NN = c(NN, nrow(full_data))
 rm(fte100)
-#Eliminate all but the highest-FTE position
+#Eliminate all but the highest-FTE position (note -- this ordering
+#  __shouldn't__ matter, because teachers should only be associated
+#  with one salary in a given year, which is __almost__ without fail the case)
 #  (eliminate 228,710 = 18%)
 full_data = full_data[order(full_time_equiv), .SD[.N],
                       by = .(teacher_id, year)]
+
+NN = c(NN, nrow(full_data))
 #Focus on Wisconsin Public Schools
 #  (eliminate 14,222 = 1.4%)
 full_data = full_data[district_work_type == '04']
+
+NN = c(NN, nrow(full_data))
 #Despite efforts to focus on full-time teachers, some,
 #  as per the months_employed construct (used pre-2003)
 #  or the days_of_contract construct (used since), still enter
@@ -82,10 +98,14 @@ full_data = full_data[district_work_type == '04']
 full_data = 
   full_data[(months_employed %between% c(875, 1050) | year > 2003) &
               (days_of_contract %between% c(175, 195) | year <= 2003)]
+
+NN = c(NN, nrow(full_data))
 #Seems unlikely a full-time teacher should be
 #  making <10k in a year, so this should be eliminated as erroneous
 #  (eliminate 807 = .08%)
 full_data = full_data[salary + fringe >= 10000]
+
+NN = c(NN, nrow(full_data))
 #Restrict focus to total experience (rounded down) between 1 & 30;
 #  Total exp = 0 appears to be some sort of error? Very rare anyway.
 #  There are nontrivial #s of teachers beyond 30 years, but 30 seems
@@ -95,6 +115,8 @@ full_data = full_data[salary + fringe >= 10000]
 #  to become important.
 #  (eliminate 78,874 = 8%)
 full_data = full_data[total_exp_floor %in% seq_len(30L)]
+
+NN = c(NN, nrow(full_data))
 #Restrict subject areas
 #  Most common excluded subject areas area:
 #  [0910] - Health
@@ -107,6 +129,7 @@ full_data = full_data[total_exp_floor %in% seq_len(30L)]
 #  (eliminate 22,577 = 2.5%)
 full_data = full_data[substring(area, 2, 2) %in% 2:7 | area == '0050']
 
+NN = c(NN, nrow(full_data))
 #Now, eliminate schools with insufficient coverage
 yrdsdg = c("year", "district_fill", "highest_degree")
 yrds = c('year', 'district_fill')
@@ -125,26 +148,22 @@ full_data[ , node_count_flag := uniqueN(total_exp_floor) < 7L, by = yrdsdg]
 full_data[ , teach_count_flag := .N < 20L, by = yrdsdg]
 
 #Also troublesome when there is little variation in salaries like so:
-full_data[ , sal_scale_flag :=
-             mean(abs(salary - mean(salary))) < 50, by = yrdsdg]
 full_data[ , sal_count_flag := uniqueN(salary) < 5L, by = yrdsdg]
-full_data[ , frn_scale_flag := 
-             mean(abs(salary - mean(fringe))) < 50, by = yrdsdg]
 full_data[ , frn_count_flag := uniqueN(fringe) < 5L, by = yrdsdg]
 
-# Impose the flag on both certification tracks
-#   whenever partially violated
-flgs = paste0(c('node_count', 'teach_count', 'sal_scale',
-                'sal_count', 'frn_scale', 'frn_count'), '_flag')
-full_data[ , (flgs) := lapply(.SD, any), by = yrds, .SDcols = flgs]
+# Impose the flag on both certification tracks whenever partially violated
+flg = paste0(c('node_count', 'teach_count', 'sal_count', 'frn_count'), '_flag')
+full_data[ , (flg) := lapply(.SD, any), by = yrds, .SDcols = flg]
 
 #Discard variables not necessary for interpolation
 #  (eliminate 189,440 = 21.5%, almost all from teach_count_flag)
 full_data = 
   full_data[!(degree_count_flag | node_count_flag | teach_count_flag |
-                sal_scale_flag | sal_count_flag |
-                frn_scale_flag | frn_count_flag),
+                sal_count_flag | frn_count_flag),
             .(year, district_fill, highest_degree,
               total_exp_floor, salary, fringe)]
+
+NN = c(NN, nrow(full_data))
+## @knitr stop_read
 
 fwrite(full_data, data.path %+% 'wisconsin_teacher_data_for_payscales.csv')

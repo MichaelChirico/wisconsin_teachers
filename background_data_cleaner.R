4c23b9f86a481081e0ab52a69d4e7476ff6e5b22
Michael Chirico <MichaelChirico4@gmail.com>
2017-03-18 17:45:18 -0400

    extracted urbanicity by year/school from CCoD

--- background_data_cleaner.R
+++ background_data_cleaner.R
@@ -9,7 +9,8 @@
 
 rm(list = ls(all = TRUE))
 gc()
-cc.d.wd = "/media/data_drive/common_core/district/"
+wds = c(cc = "/media/data_drive/common_core/school",
+        data = '/media/data_drive/wisconsin/')
 library(funchir)
 library(data.table)
 
@@ -18,5 +19,24 @@ library(data.table)
 ###############################################################################
 
 # Urbanicity (school-level, via CCD)
+## Focus on included years: 1999-2000 AY through 2007-2008 AY
+sf = grep('.*-0[0-8].*W', list.files(wds['cc'], full.names = TRUE), value = TRUE)
+names(sf) = paste0('20', gsub('.*[0-9]{4}-([0-9]{2})_.*', '\\1', sf))
+schools = rbindlist(lapply(sf, function(ff) {
+  DT = fread(ff)
+  incl_cols = grep('STID|SEASCH|LOCALE|ULOCAL', names(DT))
+  DT = DT[FIPST == '55', incl_cols, with = FALSE]
+  setnames(DT, c('district', 'school', 'urbanicity'))
+}), idcol = 'year')
 
+urban_map = data.table(
+  urbanicity = paste0(c(1:8, 11:13, 21:23, 31:33, 41:43)),
+  size = c('Large Urban', 'Small Urban', rep('Suburban', 3L),
+           rep('Rural', 3L), 'Large Urban', rep('Small Urban', 2L),
+           rep('Suburban', 3L), rep('Rural', 6L))
+)
 
+schools[urban_map, urbanicity := i.size, on = 'urbanicity']
+schools[urbanicity %in% c('M', 'N'), urbanicity := NA]
+
+fwrite(schools, wds['data'] %+% 'school_demographics.csv')

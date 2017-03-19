26a6eeb1c3ad635ebe5537e1383b186ccdf03c17
Michael Chirico <MichaelChirico4@gmail.com>
2017-03-19 15:30:43 -0400

    include ethnicity/povery measures @ district/year level

--- background_data_cleaner.R
+++ background_data_cleaner.R
@@ -9,7 +9,8 @@
 
 rm(list = ls(all = TRUE))
 gc()
-wds = c(cc = "/media/data_drive/common_core/school",
+wds = c(cc.s = "/media/data_drive/common_core/school",
+        cc.d = "/media/data_drive/common_core/district",
         data = '/media/data_drive/wisconsin/')
 library(funchir)
 library(data.table)
@@ -20,7 +21,8 @@ library(data.table)
 
 # Urbanicity (school-level, via CCD)
 ## Focus on included years: 1999-2000 AY through 2007-2008 AY
-sf = grep('.*-0[0-8].*W', list.files(wds['cc'], full.names = TRUE), value = TRUE)
+sf = grep('.*-0[0-8].*W', 
+          list.files(wds['cc.s'], full.names = TRUE), value = TRUE)
 names(sf) = paste0('20', gsub('.*[0-9]{4}-([0-9]{2})_.*', '\\1', sf))
 schools = rbindlist(lapply(sf, function(ff) {
   DT = fread(ff)
@@ -40,3 +42,26 @@ schools[urban_map, urbanicity := i.size, on = 'urbanicity']
 schools[urbanicity %in% c('M', 'N'), urbanicity := NA]
 
 fwrite(schools, wds['data'] %+% 'school_demographics.csv')
+
+# Ethnicity, poverty (district-level, via CCD)
+districts = rbindlist(lapply(sf, function(ff) {
+  DT = fread(ff)
+  incl_cols = grep('STID|SEASCH|MEMBER|^HISP|^BLACK|LCH', names(DT))
+  DT = DT[FIPST == '55', incl_cols, with = FALSE]
+  DT[ , (names(DT)) := lapply(.SD, function(x) {x[x < 0] = NA; x})]
+  setnames(DT, c('district', 'school', 'free', 
+                 'reduced', 'member', 'hisp', 'black'))
+  DT[ , lapply(.SD, sum, na.rm = TRUE), by = district,
+      .SDcols = !"school"]
+}), idcol = 'year')
+
+districts[ , frl := free + reduced]
+districts[ , c('free', 'reduced') := NULL]
+districts[member == 0, member := NA]
+
+pct_col = c('hisp', 'black', 'frl')
+districts[ , (pct_col) := lapply(.SD, `/`, member), .SDcols = pct_col]
+setnames(districts, pct_col, paste0('pct_', pct_col))
+
+# Test scores (district-level, via CCD)
+
